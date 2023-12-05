87c5d13675748d0faf7dcbf47e967722e76807d3
vikineema <victoria@kartoza.com>
2023-12-05 09:19:08 +0300

    Remove clis that are incomplete

--- deafrica_conflux/cli/main.py
+++ deafrica_conflux/cli/main.py
@@ -1,14 +1,14 @@
 import click
 
 import deafrica_conflux.__version__
-from deafrica_conflux.cli.db_to_csv import db_to_csv
-from deafrica_conflux.cli.filter_from_queue import filter_from_queue
+# from deafrica_conflux.cli.db_to_csv import db_to_csv
+# from deafrica_conflux.cli.filter_from_queue import filter_from_queue
 from deafrica_conflux.cli.publish_tasks import publish_tasks
 from deafrica_conflux.cli.rasterise_polygons import rasterise_polygons
 from deafrica_conflux.cli.run_from_queue import run_from_sqs_queue
 from deafrica_conflux.cli.run_from_txt import run_from_txt
 from deafrica_conflux.cli.save_tasks import save_tasks
-from deafrica_conflux.cli.stack import stack
+# from deafrica_conflux.cli.stack import stack
 
 
 @click.version_option(package_name="deafrica_conflux", version=deafrica_conflux.__version__)
@@ -22,6 +22,6 @@ main.add_command(save_tasks)
 main.add_command(run_from_txt)
 main.add_command(publish_tasks)
 main.add_command(run_from_sqs_queue)
-main.add_command(stack)
-main.add_command(db_to_csv)
-main.add_command(filter_from_queue)
+# main.add_command(stack)
+# main.add_command(db_to_csv)
+# main.add_command(filter_from_queue)
