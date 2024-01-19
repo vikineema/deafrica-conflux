1e2aa54fdcf96aca2cbca4bd2fd1bfece98f11ca
vikineema <vikinyaga@gmail.com>
2024-01-19 14:24:15 +0300

    Add the old version of the stack function for testing

--- deafrica_conflux/stack.py
+++ deafrica_conflux/stack.py
@@ -135,3 +135,68 @@ def stack_polygon_timeseries_to_csv(
         output_file_paths.append(output_file_path)
 
     return output_file_paths
+
+
+def stack_polygon_timeseries_to_csv_old_version(
+    polygon_ids: list[str], drill_output_directory: str | Path, output_directory: str | Path
+) -> list[str]:
+    """
+    Stack the timeseries for a polygon from the drill output parquet files
+    into a csv file. Best used in parallel processing.
+
+    Parameters
+    ----------
+    polygon_ids : list[str]
+        Unique ids for the polygons.
+    drill_output_directory : str | Path
+        Directory containing the parquet files i.e. outputs from the drill step.
+    output_directory : str | Path
+        Directory to write the csv files to.
+
+    Returns
+    -------
+    list[str]
+        File paths of the polygon(s) timeseries csv files.
+    """
+    # Support pathlib paths.
+    drill_output_directory = str(drill_output_directory)
+    output_directory = str(output_directory)
+
+    # Get the file system to use to write.
+    if check_if_s3_uri(output_directory):
+        fs = fsspec.filesystem("s3")
+    else:
+        fs = fsspec.filesystem("file")
+
+    _log.info(f"Stacking timeseries for polygons: {', '.join(polygon_ids)}")
+
+    # Find all the drill output files.
+    drill_output_files = find_parquet_files(path=drill_output_directory, pattern=".*")
+
+    # Read in all the drill output parquet files using dask dataframes.
+    df = dd.read_parquet(drill_output_files)
+
+    # Compute the timeseries for each polygon id.
+    to_compute = [df.loc[poly_id] for poly_id in polygon_ids]
+    polygons_timeseries = dask.compute(*to_compute)
+
+    # Write the timeseries to csv.
+    output_file_paths = []
+    for frame in polygons_timeseries:
+        polygon_id = frame.index[0]
+        polygon_timeseries = update_timeseries(frame)
+
+        output_file_parent_directory = os.path.join(output_directory, f"{polygon_id[:4]}")
+        output_file_path = os.path.join(output_file_parent_directory, f"{polygon_id}.csv")
+
+        if not check_dir_exists(output_file_parent_directory):
+            fs.mkdirs(output_file_parent_directory, exist_ok=True)
+            _log.info(f"Created directory: {output_file_parent_directory}")
+
+        with fs.open(output_file_path, "w") as f:
+            polygon_timeseries.to_csv(f, index_label="date")
+
+        _log.info(f"CSV file written to {output_file_path}")
+
+        output_file_paths.append(output_file_path)
+    return output_file_paths
